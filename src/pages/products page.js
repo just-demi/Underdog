6b617267d417691130fcd158855bc074f600b687
just-demi <72812729+just-demi@users.noreply.github.com>
2022-11-25 13:16:14 +0200

    Continued clean up

--- src/pages/products page.js
+++ src/pages/products page.js
@@ -21,64 +21,6 @@ const Product_page = () => {
   )
       }, []);
  
-    const products = [
-        {
-          id: "1",
-          name: "The lighting Thief",
-          price: 80,
-          quantity: 1,
-          author: "Rick Roiden"
-        },
-        {
-          id: "2",
-          name: "B grade bricks",
-          price: 10,
-          quantity: 1,
-          author: "some other supplier"
-        },
-        {
-          id: "3",
-          name: "A grade sand",
-          price: 500,
-          quantity: 1,
-          author: "some new supplier"
-        },
-        {
-          id: "4",
-          name: "A grade stones",
-          price: 500,
-          quantity: 1,
-          author: "some old supplier"
-        },
-        {
-          id: "5",
-          name: "Low strength cement",
-          price: 70,
-          quantity: 1,
-          author: "some recent supplier"
-        },
-        {
-          id: "6",
-          name: "Medium strength cement",
-          price: 75,
-          quantity: 1,
-          author: "some far supplier"
-        },
-        {
-          id: "7",
-          name: "Titans Curse",
-          price: 250,
-          quantity: 1,
-          author: "Rick Roiden"
-        },
-        {
-          id: "8",
-          name: "City of Bones",
-          price: 75,
-          quantity: 1,
-          author: "Cassandra Clare"
-        },
-      ];
 
       console.log(books);
     return (
